43ec2b1491f19911c081d85269b1d2ebdce69ff5
unknown <2023096@miu.edu.mn>
2025-10-07 13:02:39 +0800

    Updated

--- bin/hashtables.dart
+++ bin/hashtables.dart
@@ -1,8 +1,38 @@
 import 'package:hashtables/binary_search.dart';
+import 'package:hashtables/hash_search.dart';
 import 'package:hashtables/linear_search.dart';
 
 void main() {
   final myList = [42, 3, 10, 7, 28, 149, 0, 3, 25, 7, 1];
   var foundIndex = linearSearch(myList, 28);
   print(foundIndex);
+
+  myList.sort();
+  print(myList); // [0, 1, 3, 3, 7, 7, 10, 25, 28, 42, 149]
+
+  foundIndex = binarySearch(myList, 1);
+  print(foundIndex);
 }
+
+final studentList = [
+  Student(2024078, 'AZ1'),
+  Student(2024077, 'AZ2'),
+  Student(2021018, 'AD'),
+  Student(2023139, 'BB'),
+  Student(2025068, 'BS'),
+  Student(2024008, 'DD'),
+  Student(2024095, 'ES'),
+  Student(2024012, 'ED1'),
+  Student(2024054, 'EB'),
+  Student(2024081, 'ED2'),
+  Student(2024111, 'ES2'),
+  Student(2024059, 'MI'),
+  Student(2024025, 'MB'),
+  Student(2024020, 'MC'),
+  Student(2023002, 'NM'),
+  Student(2022081, 'TG'),
+  Student(2024085, 'TK'),
+  Student(2023096, 'TA'),
+  Student(2022007, 'UM'),
+  Student(2024007, 'VG'),
+];
--- lib/linear_search.dart
+++ lib/linear_search.dart
@@ -1,4 +1,4 @@
-int? linearSearch(List<int> list) {
+int? linearSearch(List<int> list, int searchValue) {
   for (int i = 0; i < list.length; i++) {
     if (list[i] == searchValue) {
       return i;
